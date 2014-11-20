814c6c167467c6b8735334a06ddedebc56362fed
Myles Borins <myles@famo.us>
2014-11-19 21:03:27 -0800

    Added bin

---
+++ bin/cmd.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*eslint no-process-exit:0*/
+
+'use strict';
+
+var path = require('path');
+
+var argv = require('minimist')(process.argv.slice(2));
+var commonGround = require('../lib');
+
+var src = argv._[0];
+var dest = argv._[1] || src;
+
+function makePath(src) {
+  if (src[0] === '/') { return src; }
+  return path.join(process.cwd(), src);
+}
+
+function help() {
+  console.log('Usage: common-ground [path to entry] [path to output(optional)]');
+  console.log('');
+  console.log('Standard Options:');
+  console.log('');
+  console.log('\t-h Show this message');
+}
+
+if (argv.h) {
+  help();
+  process.exit(0);
+}
+
+if (!src) {
+  help();
+  process.exit(1);
+}
+
+src = makePath(src);
+dest = makePath(dest);
+
+commonGround(src, dest, function (err) {
+  if (err) {
+    console.error(new Error(err));
+    process.exit(1);
+  }
+  console.log('All Done!');
+  process.exit(0);
+});
