9e1e134146f3219e3c16d2378814700bfab77ade
Myles Borins <myles@famo.us>
2015-02-02 15:29:21 -0800

    Added prepend option

--- test/test-covert.js
+++ test/test-covert.js
@@ -12,8 +12,20 @@ var example = fs.readFileSync(examplePath, 'utf8');
 var expectedPath = path.join(__dirname, 'fixtures', 'expected.js');
 var expected = fs.readFileSync(expectedPath, 'utf8');
 
+var expectedLicensePath = path.join(__dirname, 'fixtures', 'expectedLicense.js');
+var expectedLicense = fs.readFileSync(expectedLicensePath, 'utf8');
+
+var licensePath = path.join(__dirname, 'fixtures', 'license.txt');
+var license = fs.readFileSync(licensePath, 'utf8');
+
 test('convert:', function (t) {
   t.plan(1);
   var common = convert(example);
   t.equal(common, expected, 'it should have stripped require wrapper');
+});
+
+test('convert: prepended', function (t) {
+  t.plan(1);
+  var common = convert(example, license);
+  t.equal(common, expectedLicense, 'it should have stripped require wrapper and include license');
 });
